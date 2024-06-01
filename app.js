66349c8b1b2b07da2cb83430c0208740b3274ab4
Jack Willis <jwillis@sprocketsecurity.com>
2024-06-01 01:36:02 -0500

    EvenMoreUpdates

--- app.js
+++ app.js
@@ -1,63 +1,5 @@
 'use strict';
 
-const LOCALE_MESSAGES = {
-    en: {
-        timeInUTC: 'The time in UTC is:',
-        timeInCST: 'The time in CST is:',
-        timeInBRT: 'The time in BRT is:',
-        ipAddress: 'Your IP address is:',
-        locale: 'Your locale is:',
-        timeZone: 'Your time zone is:',
-        userAgent: 'Your user agent is:',
-        httpBinHeaders: 'HTTP headers from <a href="https://httpbin.org/headers">httpbin.org</a>:',
-        services: 'Global time services provided by <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
-    },
-    pt: {
-        timeInUTC: 'O horário em UTC é:',
-        timeInCST: 'O horário em CST é:',
-        timeInBRT: 'O horário em BRT é:',
-        ipAddress: 'Seu endereço IP é:',
-        locale: 'Sua localidade é:',
-        timeZone: 'Seu fuso horário é:',
-        userAgent: 'Seu agente de usuário é:',
-        httpBinHeaders: 'Cabeçalhos HTTP de <a href="https://httpbin.org/headers">httpbin.org</a>:',
-        services: 'Serviços de horário global fornecidos pelo <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
-    },
-    es: {
-        timeInUTC: 'La hora en UTC es:',
-        timeInCST: 'La hora en CST es:',
-        timeInBRT: 'La hora en BRT es:',
-        ipAddress: 'Tu dirección IP es:',
-        locale: 'Tu localidad es:',
-        timeZone: 'Tu zona horaria es:',
-        userAgent: 'Tu agente de usuario es:',
-        httpBinHeaders: 'Encabezados HTTP de <a href="https://httpbin.org/headers">httpbin.org</a>:',
-        services: 'Servicios de hora global proporcionados por <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
-    },
-    fr: {
-        timeInUTC: 'L\'heure UTC est :',
-        timeInCST: 'L\'heure à CST est :',
-        timeInBRT: 'L\'heure en BRT est :',
-        ipAddress: 'Votre adresse IP est :',
-        locale: 'Votre langue est :',
-        timeZone: 'Votre fuseau horaire est :',
-        userAgent: 'Votre agent utilisateur est :',
-        httpBinHeaders: 'En-têtes HTTP de <a href="https://httpbin.org/headers">httpbin.org</a> :',
-        services: 'Services d\'heure globale fournis par <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
-},
-};
-
-const DEFAULT_MESSAGE_LOCALE = 'en-US';
-
-const MESSAGE_LOCALE = navigator.language || DEFAULT_MESSAGE_LOCALE;
-const MESSAGE_LOCALE_MAJOR = MESSAGE_LOCALE.split('-')[0];
-const DEFAULT_MESSAGE_LOCALE_MAJOR = DEFAULT_MESSAGE_LOCALE.split('-')[0];
-const MESSAGES = LOCALE_MESSAGES[MESSAGE_LOCALE_MAJOR] || LOCALE_MESSAGES[DEFAULT_MESSAGE_LOCALE_MAJOR];
-
-const USER_TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-////////////////////////
-
 const IPIFY_API_URL = 'https://api.ipify.org?format=json';
 
 function fetchUserIp() {
@@ -68,11 +10,69 @@ function fetchUserIp() {
 
 const HTTPBIN_HEADERS_URL = 'https://httpbin.org/headers';
 
-function fetchRequestHeaders() {
+function fetchRequestHeadersFromHttpBin() {
     return fetch(HTTPBIN_HEADERS_URL)
         .then(response => response.json())
         .then(data => data.headers);
 }
+
+////////////////////////
+
+const LOCALE_MESSAGES = {
+    en: {
+        timeInUTC: 'The time in UTC is:',
+        timeInCST: 'The time in CST is:',
+        timeInBRT: 'The time in BRT is:',
+        ipAddress: 'Your IP address is:',
+        locale: 'Your locale is:',
+        timeZone: 'Your time zone is:',
+        userAgent: 'Your user agent is:',
+        httpBinHeaders: `HTTP headers from <a href="${HTTPBIN_HEADERS_URL}">${HTTPBIN_HEADERS_URL}</a>:`,
+        services: 'Global time services provided by <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
+    },
+    pt: {
+        timeInUTC: 'O horário em UTC é:',
+        timeInCST: 'O horário em CST é:',
+        timeInBRT: 'O horário em BRT é:',
+        ipAddress: 'Seu endereço IP é:',
+        locale: 'Sua localidade é:',
+        timeZone: 'Seu fuso horário é:',
+        userAgent: 'Seu agente de usuário é:',
+        httpBinHeaders: `Cabeçalhos HTTP de <a href="${HTTPBIN_HEADERS_URL}">${HTTPBIN_HEADERS_URL}</a>:`,
+        services: 'Serviços de horário global fornecidos pelo <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
+    },
+    es: {
+        timeInUTC: 'La hora en UTC es:',
+        timeInCST: 'La hora en CST es:',
+        timeInBRT: 'La hora en BRT es:',
+        ipAddress: 'Tu dirección IP es:',
+        locale: 'Tu localidad es:',
+        timeZone: 'Tu zona horaria es:',
+        userAgent: 'Tu agente de usuario es:',
+        httpBinHeaders: `Encabezados HTTP de <a href="${HTTPBIN_HEADERS_URL}">${HTTPBIN_HEADERS_URL}</a>:`,
+        services: 'Servicios de hora global proporcionados por <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
+    },
+    fr: {
+        timeInUTC: 'L\'heure UTC est :',
+        timeInCST: 'L\'heure à CST est :',
+        timeInBRT: 'L\'heure en BRT est :',
+        ipAddress: 'Votre adresse IP est :',
+        locale: 'Votre langue est :',
+        timeZone: 'Votre fuseau horaire est :',
+        userAgent: 'Votre agent utilisateur est :',
+        httpBinHeaders: `En-têtes HTTP de <a href="${HTTPBIN_HEADERS_URL}">${HTTPBIN_HEADERS_URL}</a> :`,
+        services: 'Services d\'heure globale fournis par <a href="https://www.officemsoft.com/">Office&nbsp;MSoft</a>.',
+    },
+};
+const DEFAULT_MESSAGE_LOCALE = 'en-US';
+
+const MESSAGE_LOCALE = navigator.language || DEFAULT_MESSAGE_LOCALE;
+const MESSAGE_LOCALE_MAJOR = MESSAGE_LOCALE.split('-')[0];
+const DEFAULT_MESSAGE_LOCALE_MAJOR = DEFAULT_MESSAGE_LOCALE.split('-')[0];
+
+const MESSAGES = LOCALE_MESSAGES[MESSAGE_LOCALE_MAJOR] || LOCALE_MESSAGES[DEFAULT_MESSAGE_LOCALE_MAJOR];
+
+const USER_TIMEZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 ////////////////////////
 
@@ -82,7 +82,7 @@ const Targets = {
     currentDateTimeCST: () => document.getElementById('current-date-time-cst'),
     currentDateTimeBRT: () => document.getElementById('current-date-time-brt'),
     ipAddress: () => document.getElementById('ip-address'),
-    httpbinHeaders: () => document.getElementById('httpbin-headers'),
+    httpBinHeaders: () => document.getElementById('httpbin-headers'),
 };
 
 ////////////////////////
@@ -102,8 +102,8 @@ function start() {
         Targets.ipAddress().textContent = userIpAddress;
     });
 
-    fetchRequestHeaders().then(headers => {
-        Targets.httpbinHeaders().textContent = JSON.stringify(headers, null, 4);
+    fetchRequestHeadersFromHttpBin().then(httpHeaders => {
+        Targets.httpBinHeaders().textContent = JSON.stringify(httpHeaders, null, 2);
     });
 }
 
